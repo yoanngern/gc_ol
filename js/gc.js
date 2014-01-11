8c8dba9db60ef844eca581036c66945dd55fab3e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-01-11 17:04:57 +0100

    Add click => select

--- js/gc.js
+++ js/gc.js
@@ -1,13 +1,13 @@
 var style = [new ol.style.Style({
     image: new ol.style.Circle({
-        radius: 5,
+        radius: 10,
         fill: new ol.style.Stroke({color: 'rgba(0, 0, 0, 0.2)'}),
         stroke: new ol.style.Stroke({color: 'rgba(0, 0, 0, 0.7)', width: 1})
     })
 })];
 var selected_style = [new ol.style.Style({
     image: new ol.style.Circle({
-        radius: 5,
+        radius: 10,
         fill: new ol.style.Stroke({color: 'rgba(255, 190, 0, 0.2)'}),
         stroke: new ol.style.Stroke({color: 'rgba(255, 190, 0, 0.7)', width: 1})
     })
@@ -67,8 +67,14 @@ function select(feature) {
 
 function show(feature) {
     selected_feature = feature;
-    $("#result").html("<h5>" + feature.get('name') + "</h5><p>" + feature.get('tel') + "</p>");
-    $("#result").addClass('selected');
+    if (feature) {
+        $("#result").html("<h5>" + feature.get('name') + "</h5><p>" + feature.get('tel') + "</p>");
+        $("#result").addClass('selected');
+    }
+    else {
+        selected_feature = null;
+        $("#result").removeClass('selected');
+    }
 }
 
 var first = true;
@@ -90,8 +96,7 @@ bases.addEventListener(goog.events.EventType.CHANGE, function() {
     }
 })
 $("#result").click(function() {
-    selected_feature = null;
-    $("#result").removeClass('selected');
+    show(null);
 });
 
 var geolocation = new ol.Geolocation();
@@ -113,14 +118,13 @@ geolocation.on('change:position', function(event) {
 
     var features = bases.getAllFeatures();
     var feature = null;
-    var dist = +Infinity;
+    var squaredDist = +Infinity;
     $.each(features, function() {
-        var candidate_dist = Math.sqrt(
-            Math.pow(geolocation.getPosition()[0] - this.getGeometry().getCoordinates()[0], 2),
-            Math.pow(geolocation.getPosition()[1] - this.getGeometry().getCoordinates()[1], 2)
-        );
-        if (candidate_dist < dist) {
-            dist = candidate_dist;
+        var candidateSquaredDist =
+            Math.pow(geolocation.getPosition()[0] - this.getGeometry().getCoordinates()[0], 2) +
+            Math.pow(geolocation.getPosition()[1] - this.getGeometry().getCoordinates()[1], 2);
+        if (candidateSquaredDist < squaredDist) {
+            squaredDist = candidateSquaredDist;
             feature = this;
         }
     });
@@ -128,5 +132,48 @@ geolocation.on('change:position', function(event) {
         show(feature);
     }
 });
-
 geolocation.setTracking(true);
+
+
+Select = function(opt_options) {
+  goog.base(this);
+};
+goog.inherits(Select, ol.interaction.Interaction);
+var squaredClickTolerance = 20;
+var squaredTolerance = 150;
+function clicked(point) {
+    var feature = null;
+    var squaredDist = squaredTolerance;
+    $.each(bases.getAllFeatures(), function() {
+        var candidatePoint = map.getPixelFromCoordinate(this.getGeometry().getCoordinates());
+        var candidateSquaredDist =
+            Math.pow(point[0] - candidatePoint[0], 2) +
+            Math.pow(point[1] - candidatePoint[1], 2);
+        if (candidateSquaredDist < squaredDist) {
+            squaredDist = candidateSquaredDist;
+            feature = this;
+        }
+    });
+
+    show(feature)
+    return !feature;
+};
+Select.prototype.handleMapBrowserEvent = function(event) {
+    if (event.type === ol.MapBrowserEvent.EventType.CLICK) {
+        var downPx = event.map.getEventPixel(event.target.getDown());
+        var clickPx = event.getPixel();
+        var dx = downPx[0] - clickPx[0];
+        var dy = downPx[1] - clickPx[1];
+        var squaredDistance = dx * dx + dy * dy;
+        var pass = true;
+        if (squaredDistance <= squaredClickTolerance) {
+            return clicked(clickPx)
+        }
+    }
+    if (event.type === goog.events.EventType.TOUCHEND) {
+        return clicked(event.map.getEventPixel(event));
+    }
+    return true;
+};
+
+map.addInteraction(new Select());
